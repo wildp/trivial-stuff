aa5b36f8cd7cd6c76bbef710a5d844ce54c56cd8
Peter Wild <39401784+wildp@users.noreply.github.com>
2018-09-01 08:06:22 +0100

    Update print.h

--- Bitmap Displayer/print.h
+++ Bitmap Displayer/print.h
@@ -6,7 +6,6 @@ namespace print
 	void setup();
 	void bitmap(const bmp::image &image, bmp::pixeldata &data);
 	void bitmap(const bmp::image &image, bmp::i1pixeldata &i1data);
-	//void bitmap(const bmp::image &image, bmp::i8pixeldata &i8data);
 }
 
 
