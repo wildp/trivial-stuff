3746980a5d21cba452e376e9853486cb80fba600
Peter Wild <39401784+wildp@users.noreply.github.com>
2019-08-18 18:34:38 +0100

    Create Lightcycles Infinite Launch.py
    Copied from https://github.com/wildp/lightcycles-collection/blob/master/lightcycles/Lightcycles%20Infinte%20Launch.py

---
+++ lightcycles/Lightcycles Infinite Launch.py
@@ -0,0 +1,12 @@
+#Lightcycles Infinite Launch
+
+import Lightcycles
+
+if __name__ == "__main__":
+    try:
+        while True:
+            Lightcycles.main()
+            print("\n\n\n")
+    except Exception as e:
+        print(e)
+        
